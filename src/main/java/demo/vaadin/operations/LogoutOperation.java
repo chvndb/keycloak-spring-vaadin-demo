bbfad33698b3629a25ba1c2cf9978fa93748ef54
Christophe Vandenberghe <chvndb@gmail.com>
2017-06-30 11:02:26 +0200

    Remove use of deprecated icon set

--- src/main/java/demo/vaadin/operations/LogoutOperation.java
+++ src/main/java/demo/vaadin/operations/LogoutOperation.java
@@ -2,17 +2,17 @@ package demo.vaadin.operations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.spring.security.VaadinSecurity;
-import org.vaadin.spring.sidebar.annotation.FontAwesomeIcon;
 import org.vaadin.spring.sidebar.annotation.SideBarItem;
+import org.vaadin.spring.sidebar.annotation.VaadinFontIcon;
 
-import com.vaadin.server.FontAwesome;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.spring.annotation.SpringComponent;
 
 import demo.vaadin.Sections;
 
 @SpringComponent
 @SideBarItem(sectionId = Sections.OPERATIONS, caption = "Logout")
-@FontAwesomeIcon(FontAwesome.POWER_OFF)
+@VaadinFontIcon(VaadinIcons.POWER_OFF)
 public class LogoutOperation implements Runnable {
   private final VaadinSecurity vaadinSecurity;
 
