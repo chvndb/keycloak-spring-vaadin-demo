89bb48e8fbdcb88a288fb856d5f38e6aa474d92c
Christophe Vandenberghe <chvndb@gmail.com>
2017-06-30 10:00:40 +0200

    Remove unrequired exclude

--- src/main/java/demo/vaadin/Application.java
+++ src/main/java/demo/vaadin/Application.java
@@ -2,9 +2,8 @@ package demo.vaadin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
